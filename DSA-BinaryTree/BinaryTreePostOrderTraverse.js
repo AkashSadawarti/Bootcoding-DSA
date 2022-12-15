f053d21ab996d4e38a8cd8d848c07f07dbd791b3
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-15 16:38:56 +0530

    Binary Tree PostOrder Traverse

---
+++ DSA-BinaryTree/BinaryTreePostOrderTraverse.js
@@ -0,0 +1,40 @@
+class Node{
+    constructor(data,left,right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+let root = new Node(10)
+let B = new Node(20)
+let C = new Node(30)
+let D = new Node(40)
+let E = new Node(50)
+let F = new Node(60)
+let G = new Node(70)
+let H = new Node(80)
+let I = new Node(90)
+let J = new Node(100)
+let K = new Node(110)
+
+ root.left = B
+ root.left.left = D
+ root.left.left.left = H
+ root.left.left.right = I
+ root.left.right = E
+ root.left.right.left = J
+ root.right = C
+ root.right.left = F
+ root.right.right = G
+ root.right.right.right = K
+
+ function PostOrder(root){
+    if(root == null){
+        return
+    }
+    PostOrder(root.left)
+    PostOrder(root.right)
+    console.log(`${root.data} ->`)
+}
+ PostOrder(root)
