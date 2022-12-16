d94b6a60effae0d2f731bc316fe31e41aaa156ca
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-12-16 15:21:20 +0530

    Server Update

--- Server/ServerCreation.js
+++ Server/ServerCreation.js
@@ -1,3 +1,9 @@
+//require() is used for using http module in app
+//CreateServer Name of method
+//res = response towrite message and end it
+//Web server to listen on port 1980(port num could be anything)
+
+
 let http = require('http')
 http.createServer((req,res)=>{
     res.write("Welcome to Localhost 1980's")
