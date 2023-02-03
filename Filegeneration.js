ed8bdf1c9cae8cb57ec6b4250cf29f0ed7ecbaba
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-03 20:53:59 +0530

    writefile using nodejs

---
+++ Filegeneration.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); 
+fs.writeFile('mynewfile', 'Hello content!', 
+function (err) {  
+     if (err) throw Errororor;   console.log('Saved!'); });
+
+// const fs = require('fs');
+// fs.readFile('mynewfile.txt',function(err,data){
+//     if(err) throw ERRRRRRRR; console.log('Hello Read the file' + data.toString())
+// })
+
+
+
+
